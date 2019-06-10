85a4f24403fb835a807e39a448c8acf328b6dcff
esailors2019 <51681176+esailors2019@users.noreply.github.com>
2019-06-10 16:53:21 -0700

    Add files via upload

---
+++ Erica.code.py
@@ -0,0 +1,32 @@
+# -*-jbjb
+ #coding: utf-8 -*-
+'''
+Created on Mon 10 14:11:08 2019
+
+@author: erica
+'''
+5/8 #division
+print("Hello",5, "World")
+print ("I want some soda")
+print ("I want to hear Sbvce and Baegod, Potion" )
+print (work)
+print ("5")
+5*8 #multiplication
+5-2 #subtraction
+5**3 #exponent
+8/9*3
+
+#writing code to sum any two numbers
+def sum(a,b):
+    sum= a + b
+    def sum(5,8):
+    sum= 5 + 8
+print(sum(5,8)):
+    def sum(a,b):
+        sum= a + b
+print("The sum of", a, "and", b , "is", sum)
+sum(5,8)
+    
+def sum(5,6,7):
+    sum= 5 + 6 + 7
+    print (sum(5,6,7))
